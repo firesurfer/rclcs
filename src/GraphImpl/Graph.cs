812f8f29b2a164ccf04d782cdb4a03268b9ac8b8
Lennart Nachtigall <firesurfer127@gmail.com>
2017-08-08 20:51:35 +0200

    implement codepaths for graph api

---
+++ src/GraphImpl/Graph.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+namespace rclcs
+{
+
+    public abstract class GraphBase: IDisposable
+    {
+        protected bool disposed = false;
+
+        public GraphBase()
+        {
+            
+        }
+        public void Dispose()
+		{
+			// Dispose of unmanaged resources.
+			Dispose(true);
+			// Suppress finalization.
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposed)
+				return;
+			if (disposing)
+			{
+
+				// Free any other managed objects here.
+			}
+			// Free any unmanaged objects here.
+
+
+			disposed = true;
+		}
+
+        public abstract UInt64 CountPublishers(Node RosNode, string TopicName);
+        public abstract UInt64 CountSubscriptions(Node RosNode, string TopicName);
+        public abstract RCLTopicNamesAndTypes GetTopicNamesAndTypes(Node RosNode);
+        public abstract bool ServiceServerAvailable<T, U>(Node RosNode, Client<T, U> ServiceClient)
+            where T : MessageWrapper, new()
+            where U : MessageWrapper, new();
+    }
+
+    public class Graph:IDisposable
+    {
+        GraphBase Impl;
+		protected bool disposed = false;
+
+        public Graph()
+        {
+			if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+			{
+				//TODO codepath for windows
+			}
+			else if (Environment.OSVersion.Platform == PlatformID.Unix)
+			{
+                Impl = new GraphLinux();
+			}
+			else
+			{
+				throw new Exception("Operating system: " + Environment.OSVersion.Platform.ToString() + " not supported");
+			}
+        }
+
+		public void Dispose()
+		{
+			// Dispose of unmanaged resources.
+			Dispose(true);
+			// Suppress finalization.
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposed)
+				return;
+			if (disposing)
+			{
+
+				// Free any other managed objects here.
+			}
+			// Free any unmanaged objects here.
+
+
+			disposed = true;
+		}
+
+        public UInt64 CountPublishers(Node RosNode, string TopicName)
+        {
+            return Impl.CountPublishers(RosNode, TopicName);
+        }
+
+		
+		public  UInt64 CountSubscriptions(Node RosNode, string TopicName)
+        {
+            return Impl.CountSubscriptions(RosNode, TopicName);
+        }
+		public  RCLTopicNamesAndTypes GetTopicNamesAndTypes(Node RosNode)
+        {
+            return Impl.GetTopicNamesAndTypes(RosNode);
+        }
+		public  bool ServiceServerAvailable<T, U>(Node RosNode, Client<T, U> ServiceClient)
+			where T : MessageWrapper, new()
+			where U : MessageWrapper, new()
+        {
+            return Impl.ServiceServerAvailable< T, U > (RosNode, ServiceClient);
+        }
+
+    }
+	
+	
+
+}
+
---
+++ src/NativeTypes/rcl_topic_names_and_types_t.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace rclcs
+{
+	/// <summary>
+	/// Rcl topic names and types t.
+	/// Native version 
+	/// </summary>
+	internal struct rcl_topic_names_and_types_t
+	{
+		/*
+         *  size_t topic_count;
+            char ** topic_names;
+            char ** type_names;
+         */
+		public IntPtr topic_count;
+		public IntPtr topic_names;
+		public IntPtr type_names;
+	}
+	/// <summary>
+	/// RCL topic names and types.
+	/// Managed version of the rcl_topic_names_and_types_t
+	/// </summary>
+	public class RCLTopicNamesAndTypes
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="rclcs.RCLTopicNamesAndTypes"/> class.
+		/// Takes the native_struct and marshalls the char** for topic_names and type_names
+		/// </summary>
+		/// <param name="native_struct">Native struct.</param>
+		internal RCLTopicNamesAndTypes(rcl_topic_names_and_types_t native_struct)
+		{
+			string[] topics = MarshallingHelpers.PtrToStringArray(native_struct.topic_names);
+			string[] types = MarshallingHelpers.PtrToStringArray(native_struct.type_names);
+			TopicNames = new List<string>(topics);
+			TypeNames = new List<string>(types);
+
+		}
+		public List<string> TopicNames { get; private set; }
+		public List<string> TypeNames { get; private set; }
+
+
+
+	}
+
+}
